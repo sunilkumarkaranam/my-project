fdf2728797dbcd125b40ae23f8544c040307dc94
Sunil Kumar K (MS/ETB72-PS) <uql1kor@bosch.com>
2022-07-08 17:39:02 +0530

    first commit

---
+++ callapi/src/main/java/com/bosch/callapi/CallapiApplication.java
@@ -0,0 +1,13 @@
+package com.bosch.callapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallapiApplication.class, args);
+	}
+
+}
---
+++ callapi/src/test/java/com/bosch/callapi/CallapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.callapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
